e1f86c89ed96fb5ce4d989b3a0756cde4eba1763
LawOfSynergy <stimpackken@gmail.com>
2025-07-11 15:25:23 -0700

    Add project files.

---
+++ More IC10 Commands/PCTranspiler.cs
@@ -0,0 +1,64 @@
+﻿using Assets.Scripts.Objects.Electrical;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace More_IC10_Commands
+{
+    [HarmonyPatch(typeof(ProgrammableChip._LineOfCode))]
+    [HarmonyPatch(new Type[]{typeof(ProgrammableChip), typeof(string), typeof(int)})]
+    public class PCTranspiler
+    {
+        
+
+        [HarmonyTranspiler]
+        static List<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase original)
+        {
+            List<CodeInstruction> insert = new List<CodeInstruction>();
+            Label normal = generator.DefineLabel();
+            Label extended = generator.DefineLabel();
+            LocalBuilder ext = generator.DeclareLocal(typeof(ProgrammableChip._Operation));
+
+            insert.Add(new CodeInstruction(OpCodes.Ldarg_1));
+            insert[0].labels.Add(extended);
+            insert.Add(new CodeInstruction(OpCodes.Ldarg_2));
+            insert.Add(new CodeInstruction(OpCodes.Ldarg_3));
+            insert.Add(new CodeInstruction(OpCodes.Ldloca_S, 4));
+            insert.Add(CodeInstruction.Call(typeof(Core), "LoadExtension"));
+            insert.Add(new CodeInstruction(OpCodes.Stloc, ext.LocalIndex));
+            insert.Add(new CodeInstruction(OpCodes.Ldloc, ext.LocalIndex));
+            insert.Add(new CodeInstruction(OpCodes.Brfalse, normal));
+            insert.Add(new CodeInstruction(OpCodes.Ldloc, ext.LocalIndex));
+            insert.Add(new CodeInstruction(OpCodes.Stfld, typeof(ProgrammableChip._LineOfCode).DeclaredField("Operation")));
+            insert.Add(new CodeInstruction(OpCodes.Ldarg_2));
+            insert.Add(new CodeInstruction(OpCodes.Stfld, typeof(ProgrammableChip._LineOfCode).DeclaredField("LineOfCode")));
+            insert.Add(new CodeInstruction(OpCodes.Ret));
+
+            var cm = new CodeMatcher(instructions, generator);
+
+            var count = 0;
+            cm.MatchStartForward(CodeMatch.WithOpcodes(new HashSet<OpCode> { OpCodes.Ldlen }))
+                .Repeat(cm =>
+                {
+                    count++;
+                    if (count == 2)
+                    {
+                        cm.Advance(3);
+                        cm.RemoveInstruction();
+                        cm.Insert(new CodeInstruction[] { new CodeInstruction(OpCodes.Bne_Un_S, extended) });
+                        cm.Advance(6);
+                        cm.RemoveInstruction();
+                        cm.Insert(new CodeInstruction[] { new CodeInstruction(OpCodes.Bne_Un_S, extended) });
+                        cm.Advance(12);
+                        cm.RemoveInstruction();
+                        cm.Insert(new CodeInstruction[] { new CodeInstruction(OpCodes.Bne_Un_S, extended) });
+                        cm.Advance(34);
+                        cm.Insert(insert);
+                        cm.Advance(insert.Count);
+                        cm.AddLabels(new List<Label>() { normal });
+                    }
+                });
+            return cm.Instructions();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Setup
+
+This mod involves transpiling the ProgrammableChip class, most of whose internals are private. So in order to 
+be able to access the internal classes, we have to first generate a publicized version of the dll. This dll 
+will be used for linking, but it is NOT required to replace the game's version of the dll with this publicized 
+version.
+
+## Installing the Publicizer
+
+We will be using [BepInEx.AssemblyPublicizer](https://github.com/BepInEx/BepInEx.AssemblyPublicizer/tree/master) 
+to generate our publicized dependency. I prefered to use the cli version.
+
+Install the tool with `dotnet tool install -g BepInEx.AssemblyPublicizer.Cli`
+
+## Generating Publicized Dependency
+
+In a terminal, navigate to the dependency we need to publicize. On Windows, this will be at:
+`C:\Program Files (x86)\Steam\steamapps\common\Stationeers\rocketstation_Data\Managed`
+
+Run `assembly-publicizer .\Assembly-CSharp.dll`
+
+This will create the file `Assembly-CSharp-publicized.dll`. Move this file into the publicized-dlls directory of 
+this project.
